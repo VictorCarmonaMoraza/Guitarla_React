577ccfb262709f3754dc1ecba33d1fb776a8a080
Victor_Carmona <victor.carmona@grupoica.com>
2025-03-18 20:31:37 +0100

    56-Que es JSX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
-
+import { Fragment } from "react"
+import React from 'react'
 
 export default function Header() {
 
+    const name = "Juan"
+    const total = 100
+
     return (
-        <h1>Desde Header</h1>
+        //Opcion 1
+        // <Fragment>
+        //     <p>Hola: {name}</p>
+        //     <p>Total a pagar: {total}</p>
+        // </Fragment>
+
+        // //Opcion 2
+        // <React.Fragment>
+        //     <p>Hola: {name}</p>
+        //     <p>Total a pagar: {total}</p>
+        // </React.Fragment>
+
+        //Opcion 3
+        <>
+            <p>Hola: {name}</p>
+            <p>Total a pagar: {total}</p>
+        </>
     )
 }
 
-//export default Header
