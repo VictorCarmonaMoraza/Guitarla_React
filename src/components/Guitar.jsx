b31271179d9c40767ca4bb57104b4588f04a009d
Victor_Carmona <victor.carmona@grupoica.com>
2025-03-19 08:29:09 +0100

    67-Eventos en JSX

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,13 @@
-export default function Guitar({guitar}) {
+export default function Guitar({ guitar }) {
 
-// console.log(props.price)
-// console.log(props.auth)
-const {name, price, image, description} = guitar
+    // console.log(props.price)
+    // console.log(props.auth)
+    const { id,name, price, image, description } = guitar
 
+
+    const handleClick = (guitarElement) => {
+        console.log("Agregada", guitarElement)
+    }
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
@@ -22,8 +26,11 @@ const {name, price, image, description} = guitar
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={()=>handleClick(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
     )
 }
+
+
